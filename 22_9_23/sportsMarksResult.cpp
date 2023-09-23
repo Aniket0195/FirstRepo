5d3cce7b6b78b75be62cf484f5836b737aadcc16
Aniket Gaikwad <144793942+Aniket0195@users.noreply.github.com>
2023-09-23 05:32:18 -0700

    Add files via upload

--- 22_9_23/sportsMarksResult.cpp
+++ 22_9_23/sportsMarksResult.cpp
@@ -26,7 +26,7 @@ class sports{
         this->sportsMarks = sportsMarks;
     }
     int getresult(){
-    	return student::resultMatks)+sportsMarks;
+    	return resultMatks()+sportsMarks;
 	}
 };
 
@@ -35,20 +35,21 @@ class result:public student, public sports{
     int total;
     public:
     
-    void displayResult(int rollNo, int Marks1 , intMarks1 ,int sportsMarks ):student( roll,  marks1,  marks2),sports( sportsMarks)
+    void displayResult(int rollNo, int Marks1 , int Marks2 ,int sportsMarks ):student( roll,  marks1,  marks2),sports( sportsMarks)
     {
         
-        cout<<"Avg is "<<sports::getresult()/3<<endl;
-        cout<<"total is "<< sports::getresult()<<endl;
+        cout<<"Avg is "<<getresult()/3<<endl;
+        cout<<"total is "<< getresult()<<endl;
     }
     
-}
+};
 using namespace std;
 
 int main()
 {
     
     result r1();
+    r1.displayResult();
     
 
     return 0;
---
+++ CppSDM/Rectangle.cpp
@@ -0,0 +1,20 @@
+#include "Rectangle.h"
+#include <iostream>
+using namespace std;
+
+
+	
+
+	Rectangle::Rectangle(int length, int breadth)
+	{
+	
+	    this->length = length;
+	    this-> breadth = breadth;
+	    	
+	}
+
+	
+void Rectangle:: display(){
+		int area = length * breadth;
+    cout<<"the area of Rectangle is "<<area<<endl;
+}
